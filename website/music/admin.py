88f45162abfb2a41863fc3f4a3fe003c5ab572a5
sahidagil890 <58032784+sahidagil890@users.noreply.github.com>
2020-02-05 10:16:34 +0530

    Add files via upload

--- website/music/admin.py
+++ website/music/admin.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.contrib import admin
-from .models import Album
 
-admin.site.register(Album)
+from django.contrib import admin
+
+# Register your models here.
